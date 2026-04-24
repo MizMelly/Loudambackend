e4da38edf5d12a85a8158adaec01f8bbf7cd96ef
MizMelly <jazminelike@yahoo.com>
2026-04-25 00:55:41 +0100

    mailer

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,5 +1,9 @@
 const sgMail = require("@sendgrid/mail");
 
+if (!process.env.SENDGRID_API_KEY) {
+  console.error("❌ SENDGRID_API_KEY is missing in environment variables");
+}
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = sgMail;
